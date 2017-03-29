97fab877c60e8114d04aa6a6dab70f942bfb98fb
Turupawn <ahmed.hn.43@gmail.com>
2017-03-28 19:47:49 -0600

    pantalla de cheques, fea pero funca

---
+++ app/src/main/java/intellisysla/com/vanheusenshop/entities/payment/CheckPayment.java
@@ -0,0 +1,54 @@
+package intellisysla.com.vanheusenshop.entities.payment;
+
+/**
+ * Created by turupawn on 3/28/17.
+ */
+public class CheckPayment {
+    private String checkNumber;
+
+    private String bank;
+
+    private double amount;
+
+    public CheckPayment()
+    {
+
+    }
+
+    public CheckPayment(String checkNumber, String bank, double amount)
+    {
+        this.checkNumber = checkNumber;
+        this.bank = bank;
+        this.amount = amount;
+    }
+
+    public String getCheckNumber()
+    {
+        return checkNumber;
+    }
+
+    public void setCheckNumber(String checkNumber)
+    {
+        this.checkNumber = checkNumber;
+    }
+
+    public String getBank()
+    {
+        return bank;
+    }
+
+    public void setBank(String bank)
+    {
+        this.bank = bank;
+    }
+
+    public double getAmount()
+    {
+        return amount;
+    }
+
+    public void setAmount(double amount)
+    {
+        this.amount = amount;
+    }
+}
--- app/src/main/java/intellisysla/com/vanheusenshop/ux/fragments/payment/PaymentCheckFragment.java
+++ app/src/main/java/intellisysla/com/vanheusenshop/ux/fragments/payment/PaymentCheckFragment.java
@@ -1,14 +1,42 @@
 package intellisysla.com.vanheusenshop.ux.fragments.payment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.view.ViewGroup.LayoutParams;
+
+
+import java.util.ArrayList;
 
 import intellisysla.com.vanheusenshop.R;
+import intellisysla.com.vanheusenshop.entities.payment.CheckPayment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,17 +46,37 @@ import intellisysla.com.vanheusenshop.R;
  * Use the {@link PaymentCheckFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PaymentCheckFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+class ChecksAdapter extends ArrayAdapter<CheckPayment> {
+    public ChecksAdapter(Context context, ArrayList<CheckPayment> users) {
+        super(context, 0, users);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        CheckPayment check_payment = getItem(position);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_check, parent, false);
+        }
+        TextView amount = (TextView) convertView.findViewById(R.id.amount);
+        TextView number = (TextView) convertView.findViewById(R.id.number);
+        TextView bank = (TextView) convertView.findViewById(R.id.bank);
+
+        amount.setText(getContext().getString(R.string.Amount) + ": " + check_payment.getAmount());
+        bank.setText(getContext().getString(R.string.Bank) + ": " + check_payment.getBank());
+        number.setText(getContext().getString(R.string.CheckNumber) + ": " + check_payment.getCheckNumber());
+        return convertView;
+    }
+}
+
+public class PaymentCheckFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+    ArrayList<CheckPayment> checks_array;
+    ListView my_listview;
+    RelativeLayout theLayout;
 
     public PaymentCheckFragment() {
         // Required empty public constructor
@@ -46,8 +94,7 @@ public class PaymentCheckFragment extends Fragment {
     public static PaymentCheckFragment newInstance(String param1, String param2) {
         PaymentCheckFragment fragment = new PaymentCheckFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,17 +102,89 @@ public class PaymentCheckFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_payment_check, container, false);
+        final LayoutInflater inflater_final = inflater;
+        View view = inflater.inflate(R.layout.fragment_payment_check, container, false);
+
+        final View popup = inflater_final.inflate(R.layout.popup_add_check,container,false);
+
+        final PaymentCheckFragment payment_check_fragment = this;
+
+        my_listview = (ListView)view.findViewById(R.id.check_list_view);
+
+        my_listview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                checks_array.remove(position);
+                ChecksAdapter adapter = new ChecksAdapter(payment_check_fragment.getContext(), checks_array);
+                my_listview.setAdapter(adapter);
+            }
+        });
+
+        theLayout = (RelativeLayout) view.findViewById(R.id.rl);
+
+        checks_array = new ArrayList<>();
+
+        Button add_check_button = (Button)view.findViewById(R.id.add_check_button);
+        add_check_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final PopupWindow mPopupWindow = new PopupWindow(
+                        popup,
+                        AbsListView.LayoutParams.WRAP_CONTENT,
+                        LayoutParams.WRAP_CONTENT
+                );
+
+                final EditText edit_text_amount = (EditText) popup.findViewById(R.id.amount);
+                final EditText edit_text_check_number = (EditText) popup.findViewById(R.id.check_num);
+                final EditText edit_text_bank = (EditText) popup.findViewById(R.id.bank);
+
+                edit_text_amount.setText("");
+                edit_text_check_number.setText("");
+                edit_text_bank.setText("");
+
+                mPopupWindow.setFocusable(true);
+                mPopupWindow.update();
+
+                // Set an elevation value for popup window
+                // Call requires API level 21
+                if(Build.VERSION.SDK_INT>=21){
+                    mPopupWindow.setElevation(5.0f);
+                }
+
+                Button cancelButton = (Button) popup.findViewById(R.id.button_cancel);
+                cancelButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mPopupWindow.dismiss();
+                    }
+                });
+
+                Button okButton = (Button) popup.findViewById(R.id.button_ok);
+                okButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        if(edit_text_amount.getText().toString().equals(""))
+                            return;
+
+                        checks_array.add(new CheckPayment(edit_text_check_number.getText().toString(), edit_text_bank.getText().toString(),
+                                Double.parseDouble(edit_text_amount.getText().toString())));
+                        ChecksAdapter adapter = new ChecksAdapter(payment_check_fragment.getContext(), checks_array);
+                        my_listview.setAdapter(adapter);
+
+                        mPopupWindow.dismiss();
+                    }
+                });
+                mPopupWindow.showAtLocation(theLayout, Gravity.CENTER,0,0);
+            }
+        });
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
